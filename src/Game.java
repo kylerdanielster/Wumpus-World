49fdd651cd313cf335d66e0462ec0c03570a9c5a
Kyle Daniel <kylerdanielster@gmail.com>
2015-12-01 20:51:28 -0600

    Setup the world map

--- src/Game.java
+++ src/Game.java
@@ -17,6 +17,7 @@
 
 public class Game
 {
+    private Room[][] rooms;
     private Parser parser;
     private Room currentRoom;
     private Robot player;
@@ -30,6 +31,7 @@ public class Game
     public Game()
     {
         createRooms();
+        setMapExits();
         parser = new Parser();
         player = Robot.getInstance();
         wumpus = Wumpus.getInstance();
@@ -40,7 +42,8 @@ public class Game
      */
     private void createRooms()
     {
-        Room[][] rooms = new Room[8][8];
+        // create the rooms
+        rooms = new Room[8][8];
 
         for(int i = 0; i < 8; i++)
         {
@@ -52,42 +55,59 @@ public class Game
             System.out.println();
         }
 
+        // initialize the room exits
+        // set in the constructor
 
-        Room outside, theater, pub, lab, office;
+        // start the game at 0,0
+        currentRoom = rooms[0][0];
 
-        // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
+//        Room outside, theater, pub, lab, office;
+//
+//        // create the rooms
+//        outside = new Room("outside the main entrance of the university");
+//        theater = new Room("in a lecture theater");
+//        pub = new Room("in the campus pub");
+//        lab = new Room("in a computing lab");
+//        office = new Room("in the computing admin office");
+//
+//        // initialise room exits
+//        outside.setExit("east", theater);
+//        outside.setExit("south", lab);
+//        outside.setExit("west", pub);
+//
+//        theater.setExit("west", outside);
+//
+//        pub.setExit("east", outside);
+//
+//        lab.setExit("north", outside);
+//        lab.setExit("east", office);
+//
+//        office.setExit("west", lab);
+//
+         // start game outside
+//        currentRoom = outside;
 
-        // initialise room exits
-        outside.setExit("east", theater);
-        outside.setExit("south", lab);
-        outside.setExit("west", pub);
+    }
 
-        theater.setExit("west", outside);
 
-        pub.setExit("east", outside);
+    private void setMapExits()
+    {
+         for(int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                // if statements for exits
+                if( i - 1 >= 0)// if row > 0
+                    rooms[i][j].setExit("north", rooms[i-1][j]);
+                if( i < 7 )//if rom < 7
+                    rooms[i][j].setExit("south", rooms[i+1][j]);
+                if( j - 1 >= 0)//if col > 0
+                    rooms[i][j].setExit("west", rooms[i][j-1]);
+                if(j < 7)//if col < 7
+                    rooms[i][j].setExit("east", rooms[i][j+1]);
+            }
+        }
 
-        lab.setExit("north", outside);
-        lab.setExit("east", office);
-
-        office.setExit("west", lab);
-
-        currentRoom = outside;  // start game outside
-
-        /********************************************
-         *                                          *
-         * BASIC TEST OF THE ROOM SIGNALS AND ITEMS *
-         *                                          *
-         *******************************************/
-        outside.addItem(new BasicItem());
-        System.out.println(outside.getItems().get(0).getDescription());
-
-        outside.addSignal(new BasicSignal());
-        System.out.println(outside.getSignals().getSignal());
     }
 
     /**
